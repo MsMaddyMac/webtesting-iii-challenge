c9a26107a0edbf0de88defb489ddeb6bcf374ec8
Maddy <mmcintosh2k14@gmail.com>
2019-11-11 20:25:02 -0500

    adds fireEvent tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,9 +1,21 @@
-// // Test away!
+// // Test imports!
 import React from "react";
-import { render } from 'react-testing-library';
+import { render, fireEvent } from 'react-testing-library';
 
 import Controls from './Controls';
 
-test('it renders correctly', () => {
-    expect(render(<Controls />)).toMatchSnapshot();
-});
+
+// Tests
+test('If unlock button clicked, can not open gate', () => {
+    const wrapper = render(<Controls />);
+    const unlockButton = wrapper.getByTestId('unlock-button');
+    const closeButton = wrapper.getByTestId('close-button');
+    // expect(unlockButton.textContent).toBe(`Lock Gate`);
+    expect(closeButton.textContent).toBe(`Close Gate`);
+    // fireEvent.click(unlockButton);
+    // expect(closeButton.textContent).toBe('Close Gate');
+    fireEvent.click(closeButton);
+    expect(unlockButton.textContent)
+    .toBe(`Lock Gate`);
+    expect(closeButton.textContent).toBe(`Close Gate`)
+})
